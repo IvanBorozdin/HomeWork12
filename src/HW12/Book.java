f3829ec414a236b105e1db6826bfc08825e8a0b8
IvanBorozdin <120761206+IvanBorozdin@users.noreply.github.com>
2023-02-04 17:24:57 +0700

    Revert "HW12.1"

--- src/HW12/Book.java
+++ src/HW12/Book.java
@@ -2,8 +2,6 @@ package HW12;
 
 import HW12.Author;
 
-import java.util.Objects;
-
 public class Book {
     public String nameBook;
     public Author author;
@@ -25,27 +23,5 @@ public class Book {
     }
     public void setYear(int year){
         this.year=year;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "nameBook='" + nameBook + '\'' +
-                ", author=" + author +
-                ", year=" + year +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Book book = (Book) o;
-        return year == book.year && Objects.equals(nameBook, book.nameBook) && Objects.equals(author, book.author);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(nameBook, author, year);
     }
 }
