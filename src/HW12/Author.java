36462c97b579a99af29dc856404e293dadc147b3
IvanBorozdin <120761206+IvanBorozdin@users.noreply.github.com>
2023-02-05 14:58:37 +0700

    Домашняя работа 12.1

--- src/HW12/Author.java
+++ src/HW12/Author.java
@@ -30,7 +30,7 @@ public class Author {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Author author = (Author) o;
-        return Objects.equals(name, author.name) && Objects.equals(surname, author.surname);
+        return Objects.equals( name, author.name) && Objects.equals(surname, author.surname);
     }
 
     @Override
